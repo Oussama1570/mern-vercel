8c4c3d17062a5d45ffe6059d113e791481b1326a
Oussama <Messamessus@yahoo.com>
2025-02-21 17:29:01 +0100

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173','https://mern-vercel-ebon.vercel.app/'  ],
+    origin: ['http://localhost:5173','https://mern-vercel-2vjq.vercel.app/'  ],
     credentials: true
 }))
 
